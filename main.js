9c674a2d3bae407be28ae96260bea49ddc0d641f
Paradox Programming <nathaniel.fargo@gmail.com>
2020-03-22 21:10:51 -0600

    more stuff

--- main.js
+++ main.js
@@ -6,6 +6,46 @@ var config = {
     },
     virus: {
         length: 50,
-        onlyOnce: true
+        onlyOnce: true,
     }
 };
+
+var Human = function(id, type) {
+    this.x = random(width);
+    this.y = random(height);
+    var _direction = random(360);
+    this.stage = 1;
+    this.id = id;
+    this.handled = [];
+};
+Human.prototype.draw = function() {
+
+};
+Human.prototype.update = function() {
+    this.handled = [];
+    if(this.type == "move") {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+};
+Human.prototype.handle = function(anotherHuman) {
+    // Check if collision isn't needed
+    var alreadyHandled = this.handled.some(function(id) {
+        return id == anotherHuman.id;
+    });
+    if(alreadyHandled) {
+        return;
+    }
+    anotherHuman.handled.push(this.id);
+    if (dist(this.x, this.y, anotherHuman.x, anotherHuman.y) > this.radius + anotherHuman.radius) {
+        return;
+    }
+    // Alright a collision has been detected, now we handle it
+    
+};
+
+
+var draw = function() {
+    background(200, 220, 250);
+    
+}
--- p5setup.js
+++ p5setup.js
@@ -3,9 +3,12 @@ function random(low, high) {
         return Math.random() * (high - low) + low;
     } else if (low) {
         return Math.random() * low;
+    } else {
+        return Math.random();
     }
 }
-
+var width = 400, height = 400;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(width, height);
+    angleMode(DEGREES);
 }
